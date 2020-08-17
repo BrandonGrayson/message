553d239f0d87fd793707a8fa32f96fac37e6cd98
Brandon Grayson <brandonbgrayson@gmail.com>
2020-08-16 21:39:49 -0400

    Url Message Encryption

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+// select the form 
+
 document.querySelector('form').addEventListener('submit', event => {
     event.preventDefault();
 
+    // Select input generate encrypted value
     const input = document.querySelector('#message-input');
     const encrypted = btoa(input.value);
 
-    document.querySelector('#link-input').value = encrypted;
+    // Designate 1st input value  as second inputs value
+    const linkInput = document.querySelector('#link-input')
+    linkInput.value = `${window.location}#${encrypted}`;
+    linkInput.select();
 });
